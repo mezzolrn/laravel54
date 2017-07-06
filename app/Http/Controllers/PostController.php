e88b9f0f0222c7d40177f0f56e31e574610c7416
mezzolrn <mezzolrn@foxmail.com>
2017-07-06 15:32:35 +0800

    blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,19 +9,30 @@ class PostController extends Controller
 	//列表
     public function index()
     {
-    	return view("post/index");
+    	$posts = [
+    		[
+    			'title' => 'this is title1',
+    		],
+    		[
+    			'title' => 'this is title2',
+    		],
+    		[
+    			'title' => 'this is title3',
+    		],
+    	];
+    	return view("post/index",compact('posts'));
     }
 
     //详情页面
     public function show()
     {
-
+    	return view("post/show",['title' => 'this is title','isShow' => false]);
     }
 
     //创建页面
     public function create()
     {
-
+    	return view("post/create");
     }
 
     //创建逻辑
@@ -33,7 +44,7 @@ class PostController extends Controller
     //编辑页面
     public function edit()
     {
-
+    	return view("post/edit");
     }
 
     //编辑逻辑
--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,17 @@
 
 //文章列表页
 Route::get('/posts', '\App\Http\Controllers\PostController@index');
-//文章详情页
-Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
 //创建文章
 Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
 Route::post('/posts', '\App\Http\Controllers\PostController@store');
+
 //编辑文章
-Route::get('/posts/{post}/edit}', '\App\Http\Controllers\PostController@edit');
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
 Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
 //删除文章
 Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
 
-
+//文章详情页
+Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
 
 //Route::get('/', '控制器@方法'});
