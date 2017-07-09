f8a6796454e874c4c94a26521c0811bcb3025207
mezzolrn <mezzolrn@foxmail.com>
2017-07-10 00:31:34 +0800

    4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,30 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Post;
 
 class PostController extends Controller
 {
 	//列表
     public function index()
     {
-    	$posts = [
-    		[
-    			'title' => 'this is title1',
-    		],
-    		[
-    			'title' => 'this is title2',
-    		],
-    		[
-    			'title' => 'this is title3',
-    		],
-    	];
+    	$posts = Post::orderBy('created_at','desc')->paginate(6);
     	return view("post/index",compact('posts'));
     }
 
     //详情页面
-    public function show()
+    public function show(Post $post)
     {
-    	return view("post/show",['title' => 'this is title','isShow' => false]);
+    	return view("post/show",['post'=>$post]);
     }
 
     //创建页面
@@ -38,24 +29,57 @@ class PostController extends Controller
     //创建逻辑
     public function store()
     {
+    	//验证
+    	$this->validate(request(),[
+    		'title' => 'required|string|max:100|min:5',
+    		'content' => 'required|string|min:10'
+    	]);
 
+    	//逻辑
+    	$post = Post::create(request(['title', 'content']));
+
+    	//渲染
+    	return redirect("/posts");
     }
 
     //编辑页面
-    public function edit()
+    public function edit(Post $post)
     {
-    	return view("post/edit");
+    	return view("post/edit",compact('post'));
     }
 
     //编辑逻辑
-    public function update()
+    public function update(Post $post)
     {
+    	//验证
+    	$this->validate(request(),[
+    		'title' => 'required|string|max:100|min:5',
+    		'content' => 'required|string|min:10'
+    	]);
 
+    	//逻辑
+    	$post->title = request('title');
+    	$post->content = request('content');
+    	$post->save();
+
+    	//渲染
+    	return redirect("/posts/{$post->id}");
     }
 
     //删除逻辑
-    public function delete()
+    public function delete(Post $post)
     {
+    	//tode用户权限验证
 
+    	$post->delete();
+
+    	return redirect("/posts");
+    }
+
+    //图片上传
+    public function imageUpload(Request $request)
+    {
+    	$path = $request->file('wangEditorH5File')->storePublicly(md5(time()));
+    	return asset('storage/'.$path);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ Route::get('/posts', '\App\Http\Controllers\PostController@index');
 //创建文章
 Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
 Route::post('/posts', '\App\Http\Controllers\PostController@store');
-
 //编辑文章
 Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
 Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
 //删除文章
-Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
-
+Route::get('/posts/{post}/delete', '\App\Http\Controllers\PostController@delete');
 //文章详情页
 Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
+//图片上传
+Route::post('/posts/image/upload','\App\Http\Controllers\PostController@imageUpload');
 
 //Route::get('/', '控制器@方法'});
